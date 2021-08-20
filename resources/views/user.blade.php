5cad0528e47e9dd26c372963ea2cb44773108280
putrialifiaazzahra <putrialifiaa412@gmail.com>
2021-08-20 12:04:54 +0700

    Update baru

---
+++ resources/views/user.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+   
+<div class="container mt-5">
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card border-0 shadow rounded">
+                <div class="card-body">
+                    <a href="" class="btn btn-md btn-primary mb-3">+ Tambah Blog</a>
+                    <table class="table table-bordered">
+                        <thead  class="text-center">
+                          <tr>
+                            <th scope="col">Picture</th>
+                            <th scope="col">Tittle</th>
+                            <th scope="col">Content</th>
+                            <th scope="col">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @forelse ($blogs as $blog)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ Storage::url('public/blogs/').$blog->image }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $blog->title }}</td>
+                                    <td>{!! $blog->content !!}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('blog.destroy', $blog->id) }}" method="POST">
+                                            <a href="{{ route('blog.edit', $blog->id) }}" class="btn btn-sm btn-success">EDIT</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                              @empty
+                                  <div class="alert alert-danger">
+                                      Data Blog belum Tersedia.
+                                  </div>
+                              @endforelse --}}
+                        </tbody>
+                      </table>
+                    {{-- {{ $blogs->links() }} --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+   
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 });
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin', function () { return view('admin'); })->middleware('checkRole:admin');
+Route::get('user', function () { return view('user'); });
